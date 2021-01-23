96e1561b28827ce2e1e02d3aac26f91d151556bc
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2021-01-23 11:24:21 -0600

    Detect Microsoft Streams

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 // Main Method
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log("Detected a match!");
     chrome.tabs.sendMessage(tabId, {"message": "nyaa.si"});
 });
